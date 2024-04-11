315dc8a1d6993e966538e5e234bcd3107e894aad
Omar Alkanj <Omar.Alkanj@isi.com>
2024-04-11 10:40:12 +0200

    added DBConnector

---
+++ utility/DbConnector.js
@@ -0,0 +1,24 @@
+const sql = require('mssql')
+
+
+class DbConnector
+{
+
+async GetConnection()
+ {
+    try {
+        // make sure that any items are correctly URL encoded in the connection string
+        await sql.connect('Server=localhost,1433;Database=database;User Id=username;Password=password;Encrypt=true')
+        const result = await sql.query`select * from mytable where id = ${value}`
+        console.dir(result)
+    } catch (err) {
+        // ... error checks
+    }
+
+ }
+
+}
+
+
+const conn = new DbConnector();
+module.exports = conn;
