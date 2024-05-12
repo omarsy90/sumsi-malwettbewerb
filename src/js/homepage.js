b1ad2048e9c36a5f1372bcda66870cb6ba21df9e
omar <edualkoma001@fh-kaernten.at>
2024-05-12 19:28:38 +0200

    added press Like functionality

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -1,10 +1,12 @@
 
 const bilderSection = document.getElementById('bilder');
-
-
+bilderSection.innerHTML = '';
+let SelectedSubmissionID = -1 ; 
+DisplaySubmission() ; 
 let countBilder = 6;
 
-function printAllFotos() {
+function DisplaySubmission() 
+{
 
     async function getAllsubmission() {
 
@@ -22,52 +24,95 @@ function printAllFotos() {
     getAllsubmission()
         .then(response => {
             const arrayData = response.data;
-            if (arrayData.length > 0) {
-
-                bilderSection.innerHTML = '';
-                for (let i = 0; i < arrayData.length; i++) {
-                  
-             
-
-                   
-
-                       bilderSection.innerHTML = bilderSection.innerHTML + `
-                         <div class="element"> 
-                         <a href="./bilder.html?sub_id=${arrayData[i].SubmissionID}"> <div class="bild" id =\"${arrayData[i].SubmissionID}\"  ></div> 
-                         </a> 
-                         <div class="bilder_element_footer">
-                          <button class="like__btn" data-submissionID="${arrayData[i].SubmissionID}"  onclick="LikeBtnClicked(event)"> <span id="icon"><i class="far fa-thumbs-up"></i></span> 
-                          <span id="count">${arrayData[i].LikeCount}</span> Like </button>
-                           <div class="kind_name">${arrayData[i].ChildName}</div> 
-                           </div> </div>  `;
-
-                        bildDiv = document.getElementById(`${arrayData[i].SubmissionID}`);
-                        bildDiv.style.backgroundImage = `url("/uploads/${arrayData[i].ImgName}")`;
-                        bildDiv.style.backgroundSize = 'cover';
-                        bildDiv.style.backgrounRepeat = 'no-repeat';
-                        
-
-
-                    
-
-                }
-
-
+            if (arrayData.length > 0)
+            {
+                
+                for (let i = 0; i < arrayData.length; i++)
+                     {
+                        ResolveSubmissionInHtml(bilderSection,arrayData[i])
+                    }
             }
-
-
-
         }).catch(error => {
               console.log(error)
         });
 
 }
 
-printAllFotos() ; 
-function LikeBtnClicked(event)
+
+ function LikeBtnClicked(event)
 {
-   console.log(event.target.getAttribute('data-submissionID'))
+   console.log(event.target.getAttribute('data-submissionID'));
+   SelectedSubmissionID = event.target.getAttribute('data-submissionID');
 }
+
+
+async function ConfirmAddingLike()
+{
+  const spinner = document.getElementsByClassName('spinner-border')[0];
+  const closeBtn = document.getElementsByClassName('btn btn-secondary')[0];
+  const email = document.getElementById('email').value ;
+  spinner.style.visibility = 'visible';
+  closeBtn.style.visibility = 'hidden';
+  const data = {email : email.replace(' ','')}
+
+  response = await fetch(`http://localhost:3000/voting/${SelectedSubmissionID}`,
+  {method:'post',headers:{
+  'Content-Type' : 'application/json'
+  },
+  body:  JSON.stringify(data)
+}
+);
+
+if(response.status === 200)
+{
+  await  UpdateLikeCountForSubmission(SelectedSubmissionID)
+}else if(response.status ===400)
+    {  
+        window.alert('check the given email . be aware that it is only allowd one like per on submission and per email ')
+    }else if(response.status ===401)
+        {
+            window.alert('the email should not be empty !!')
+        }
+  
+  document.getElementById('email').value= '' ;
+  spinner.style.visibility = 'hidden';
+  closeBtn.style.visibility = 'visible';
+closeBtn.click();
+
+}
+
+async function UpdateLikeCountForSubmission(submissionID)
+{
+   const span=  document.querySelector(`span[data-submissionID="${submissionID}"]`);
+   const res =  await fetch (`http://localhost:3000/submission/${submissionID}`) ;
+       const resData = await res.json()
+       const newLikeCount = resData.data[0].LikeCount
+     span.textContent = newLikeCount+'';
+}
+
+
+function ResolveSubmissionInHtml( htmlSection,submission)
+{
+    htmlSection.innerHTML +=  `
+    <div class="element" data-submissionID="${submission.SubmissionID}" > 
+    <a href="./bilder.html?sub_id=${submission.SubmissionID}"> <div class="bild" id =\"${submission.SubmissionID}\"  ></div> 
+    </a> 
+    <div class="bilder_element_footer">
+     <button class="like__btn" data-submissionID="${submission.SubmissionID}"  data-toggle="modal" data-target="#exampleModalCenter" onclick="LikeBtnClicked(event)"  >
+      <span >
+     <i class="far fa-thumbs-up"></i>
+     </span> 
+     <span  data-submissionID="${submission.SubmissionID}" >${submission.LikeCount}</span> Like 
+     </button>
+      <div class="kind_name">${submission.ChildName}</div> 
+      </div> </div>  `;
+   bildDiv = document.getElementById(`${submission.SubmissionID}`);
+   bildDiv.style.backgroundImage = `url("/uploads/${submission.ImgName}")`;
+   bildDiv.style.backgroundSize = 'cover';
+  bildDiv.style.backgrounRepeat = 'no-repeat';
+}
+
+  
 
 
 
